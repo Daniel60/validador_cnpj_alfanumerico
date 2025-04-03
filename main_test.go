55135a50ad8c3d9ae483d26c0c1a3a32533c59f9
Daniel60 <39488923+Daniel60@users.noreply.github.com>
2025-04-03 16:17:23 -0300

    commit inicial

---
+++ main_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/stretchr/testify/assert"
+)
+
+type TestStructCNPJ struct {
+	Document string `validate:"cnpj"`
+}
+
+func TestIsCNPJ(t *testing.T) {
+	validate := validator.New(validator.WithRequiredStructEnabled())
+	validate.RegisterValidation("cnpj", ValidadorCNPJ)
+
+	tests := []struct {
+		name     string
+		document string
+		expected bool
+	}{
+		{"Valido CNPJ", "12345678000195", true},
+		{"Valido CNPJ com mascara", "12.345.678/0001-95", true},
+		{"Valido CNPJ alfanumerico", "28UV8YY9000174", true},
+		{"Valido CNPJ alfanumerico com mascara", "28.UV8.YY9/0001-74", true},
+		{"Invalido CNPJ", "12345678900111", false},
+		{"Invalido CNPJ alfanumerico", "28.UV8.YY9/0001-70", false},
+		{"Invalido CNPJ alfanumerico com mascara", "28.UV8.YY9/0001-70", false},
+		{"Invalido CNPJ alfanumerico com mascara", "28.UV8.YY9/0001-70", false},
+		{"Invalido todos os digitos iguais", "1111111111111", false},
+		{"Invalido vazio", "", false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			testStruct := TestStructCNPJ{
+				Document: tt.document,
+			}
+			err := validate.Struct(testStruct)
+			if tt.expected {
+				assert.NoError(t, err)
+			} else {
+				assert.Error(t, err)
+			}
+		})
+	}
+}
